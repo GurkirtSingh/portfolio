4b393f051722d77c85f7406cd7cb3f465ca87431
GurkirtSingh <GurkirtSingh143@gmail.com>
2023-08-11 00:36:26 -0700

    Update layout.tsx
    Added background component

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Background from './background/page'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <body style={{margin:0, overflow: 'hidden'}} className={inter.className}>
+      <Background>{children}</Background>
+    </body>
   )
 }
