e9791def1834f5388e0ddd7ea2b675a8a33c7510
GurkirtSingh <GurkirtSingh143@gmail.com>
2023-08-11 00:38:10 -0700

    organized Homepage
    added components to Homepage

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,29 @@
+import Navbar from "./navbar/page";
+import "./page.module.css"
+import ProfilePicture from "./profile_picture/page";
+import SocialLinks from "./social_links/page";
 export default function Home(){
   return (
   <>
-    <h1>Hello World!</h1>
+    <Navbar/>
+    <div style={{
+      height: "70vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+    }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignItems: "center"
+      }}>
+        <ProfilePicture/>
+        <h3>An Experienced Full Stack Developer </h3>
+      </div>
+      <SocialLinks/>
+    </div>
+    
   </>);
 }
